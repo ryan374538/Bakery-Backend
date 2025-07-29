f00f1246080dd4ea73850800de3bfc67abec26a9
kennnedytz <mrkennito@gmail.com>
2025-07-29 16:16:53 +0300

    changed connection

--- connection.py
+++ connection.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-username="root"
-password="2480"
-database="order_db"
+username = "root"
+password = "2480"
+database = "order_db"
 
-path=f"mysql+pymysql://{username}:{password}@localhost/{database}"
+# Change the driver and dialect to PostgreSQL
+path = f"postgresql+psycopg2://{username}:{password}@localhost/{database}"
 
-data=create_engine(path)
+data = create_engine(path)
 
-Session=sessionmaker(bind=data)
+Session = sessionmaker(bind=data)
 
-session=Session()
+session = Session()
